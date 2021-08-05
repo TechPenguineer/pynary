5afa8447a575290c2534eef833ae64e9519455c4
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 00:05:05 -0400

    special signs binary table

---
+++ data/table.py
@@ -0,0 +1,18 @@
+# Special Symbol
+space=100000
+exlamation_point=100001
+double_quote=100010
+pound=100011
+dollarsign=100100
+precentage_sign=100101
+at_sign=100110
+single_quote=100111
+left_parenthese=101000
+right_parenthese=101001
+star=101010
+addidtion=101011
+comma=101100
+dash=101101
+period=101110
+forward_slash=101111
+
